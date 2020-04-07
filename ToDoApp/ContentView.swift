10ef91d5c851df4bea28a071b130d64622b9a6d9
Jared Davidson (Archetapp) <archetapp@gmail.com>
2020-04-06 18:16:53 -0600

    Initial Commit

---
+++ ToDoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoApp
+//
+//  Created by Jared on 4/6/20.
+//  Copyright © 2020 Davidson Family. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
